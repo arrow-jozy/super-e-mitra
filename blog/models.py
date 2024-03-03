11592fc8d46a7dae53b3a08cd96fe02b3ccab0f2
imroz-jozy <imrozk33@gmail.com>
2024-03-03 10:37:09 +0530

    update

--- blog/models.py
+++ blog/models.py
@@ -7,7 +7,9 @@ from django.utils import timezone
 class Post(models.Model):
     sno=models.AutoField(primary_key=True)
     title=models.CharField(max_length=100)
-    author=models.CharField(max_length=50)
+    desc=models.CharField(max_length=200,blank=True)
+    category=models.CharField(max_length=50,blank=True)
+    author=models.CharField(max_length=50,blank=True)
     content=models.TextField()
     blogtype=models.CharField(max_length=100,blank=True)
     slug=models.CharField(max_length=150)
